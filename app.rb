6edd111e42c4169346a3a1a34f63404f45dcbee4
latmannen <163900633+latmannen@users.noreply.github.com>
2024-04-28 16:35:49 +0000

    finishing up project

--- app.rb
+++ app.rb
@@ -2,8 +2,50 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+get("/rock") do
+  moves = ["rock","paper","scissors"]
+  @our_move = "rock"
+  @comp_move = moves.sample
+  
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  erb(:rock)
+end
+
+get("/paper") do
+  moves = ["rock","paper","scissors"]
+  @our_move = "paper"
+  @comp_move = moves.sample
+  
+  if @comp_move == "paper"
+    @outcome = "tied"
+  elsif @comp_move == "scissors"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  erb(:rock)
+end
+
+get("/scissors") do
+  moves = ["rock","paper","scissors"]
+  @our_move = "scissors"
+  @comp_move = moves.sample
+  
+  if @comp_move == "scissors"
+    @outcome = "tied"
+  elsif @comp_move == "rock"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  erb(:rock)
 end
